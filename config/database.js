cc753f1e287f859813eeae4546f30b546e4d4679
Will Hinkelman <will.hink24@gmail.com>
2019-12-10 13:56:05 -0800

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/bonus',
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+
+// shortcut to mongoose.connection object
+var db = mongoose.connection;
+
+db.on('connected', function () {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
